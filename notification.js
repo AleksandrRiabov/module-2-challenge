fb41eb54a779944cf837ff95dd10aa55e4fce8ba
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2022-12-04 20:31:17 +0000

    Work section complete

---
+++ notification.js
@@ -0,0 +1,5 @@
+const planningToolProject = document.querySelector('.secondP');
+
+planningToolProject.addEventListener('click', () => {
+  alert("Unfortunatly Heroku stop providing free hosting, and I won't be able to show you this project live at the moment.. But I'll take you to GitHub repo of this project. :)")
+});
