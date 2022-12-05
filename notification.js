c2c9339f055a599ca684fea8d04c7d9b3e126b19
Aleksandr Riabov <aleksandr.riabov@gmail.com>
2022-12-05 22:25:38 +0000

    Media queries breakpoint added

--- notification.js
+++ notification.js
@@ -1,5 +1,9 @@
 const planningToolProject = document.querySelector('.secondP');
 
 planningToolProject.addEventListener('click', () => {
-  alert("Unfortunately, Heroku stopped providing free hosting, and I won't be able to show you this project live at the moment. But I'll take you to the GitHub repo of this project. :)")
+ let answer = confirm("Unfortunately, Heroku stopped providing free hosting, and I won't be able to show you this project live at the moment. But I'll take you to the GitHub repo of this project. :)")
+if (answer) {
+  window.location.href = 'https://github.com/AleksandrRiabov/fs-planning-tool'
+}
 });
+
